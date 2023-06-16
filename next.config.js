89cf7f177e78ab976788653ea48bd4256b984b07
Adnan Saify <adnansaify11@gmail.com>
2023-06-17 00:28:39 +0530

    added env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    MONGODB_URI:
+      "mongodb+srv://ecommerce:flip215272@ecommerce.fccpr22.mongodb.net/?retryWrites=true&w=majority",
+    GOOGLE_ID:
+      "944364658665-7uc1vi6f9i7t2lkt2gks28stcipd0s44.apps.googleusercontent.com",
+    GOOGLE_SECRET: "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",
+    S3_ACCESS_KEY: "AKIAWM2WXZCGNHZSW5JX",
+    S3_SECRET_ACCESS_KEY: "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
